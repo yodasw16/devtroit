7e8680e3e7ffeab59a2ff3601160efe9f3c7fa6a
dave gillhespy <yodasw16@gmail.com>
2012-02-13 12:04:35 -0500

    version 1 release

--- process.php
+++ process.php
@@ -1,35 +1,21 @@
 <?php
+if ($_POST['name']) {
 
-$filename = "form.txt"; #CHMOD to 666
-$forward = 0; # redirect? 1 : yes || 0 : no
-## set time up ##
+    $file_name = "form.txt";
+    $open_file = fopen($file_name, "a+");
 
-$date = date("l, F jS, Y");
-$time = date("h:i A");
+    $file_contents = $_POST['name'] . "," . $_POST['url'] . "\n";
 
-## mail message ##
+    fwrite($open_file, $file_contents);
 
-$msg = "Below is the result of your feedback form. It was submitted on $date at $time.\n\n";
+    fclose($open_file);
 
-foreach ($_POST as $key => $value) {
-    $msg .= ucfirst($key) . " : " . $value . "\n";
-}
-
-$msg .= "-----------\n\n";
-
-$fp = fopen($filename, "a"); # w = write to the file only, create file if it does not exist, discard existing contents
-if ($fp) {
-    fwrite($fp, $msg);
-    fclose($fp);
-} else {
-    $forward = 2;
-}
-
-if ($forward == 1) {
-    header("Location:$location");
-} else if ($forward == 0) {
-    echo ("Thank you for submitting our form. We will get back to you as soon as possible.");
-} else {
-    "Error processing form. Please contact the webmaster";
+//    echo "Form data successfully written to file";
+    
+    $response = array(
+        message => 'success'
+    );
+    
+    return json_encode($response);
 }
 ?>
