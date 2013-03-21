39e2cd8a08889e958e39a72dbe76c3b170085a52
Dave Gillhespy <yodasw16@gmail.com>
2013-03-20 23:28:53 -0400

    initial file setup

---
+++ config.rb
@@ -0,0 +1,17 @@
+# Require any additional compass plugins here.
+require "susy"
+require "modular-scale"
+
+# Set this to the root of your project when deployed:
+http_path = "/"
+css_dir = "/css"
+sass_dir = "./"
+
+# You can select your preferred output style here (can be overridden via the command line):
+output_style = :compact
+
+# To enable relative paths to assets via compass helper functions. Uncomment:
+relative_assets = true
+
+# To disable debugging comments that display the original location of your selectors. Uncomment:
+# line_comments = false
